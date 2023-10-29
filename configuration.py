1ee29e5d1112bc2fa9a6794de21c82fdaa91ee14
mn-24@U1530EM <mn240850@gmail.com>
2023-10-29 20:31:41 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,2 @@
+URL_SERVICE = https://565db555-f640-4c8a-beb3-98654084c559.serverhub.praktikum-services.ru
+DOC_PATH = "/docs/"
---
+++ sender_stand_request.py
@@ -0,0 +1,11 @@
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH,
+                        params={"count": 20})
+
+
+response = get_logs()
+print(response.status_code)
+print(response.headers)
+
+
+
